ca4dd8a6436821f0849e06907b9d4e5b18459bb7
Prompt-Sopa <agustinalsop@gmail.com>
2025-05-13 18:44:47 -0300

    Update Ejer 2

--- ejercicio2/rock-paper-scissors.py
+++ ejercicio2/rock-paper-scissors.py
@@ -1,3 +1,16 @@
+############################################################################################
+#                                                                                          #
+#                               APRENDIZAJE AUTOMÁTICO 2                                   #
+#                                 TRABAJO PRÁCTICO N°1                                     #
+#                                                                                          #
+#                GRUPO N°12: Ulises Nemeth, Agustín Alsop, Rocío Hachen                    #
+#                                                                                          #
+#                          Problema 2 - Piedra, Papel y Tijera                             #
+#                                                                                          #
+#                                   Detección en vivo                                      #
+############################################################################################
+
+# --- LIBRERÍAS ---
 import cv2
 import time
 import numpy as np
@@ -5,11 +18,11 @@ import mediapipe as mp
 from tensorflow.keras.models import load_model
 
 # --- CONFIGURACIÓN ---
-model = load_model("rps_model.h5")
+model = load_model("./modelo/rps_model.h5")
 RESIZE_DIM = (320, 240)
-label_map = {'r': 0, 'p': 1, 's': 2}
-labels = ['Piedra', 'Papel', 'Tijera']
-# MediaPipe
+LABELS = ['Piedra', 'Papel', 'Tijera']
+
+# --- MEDIAPIPE ---
 mp_hands = mp.solutions.hands
 hands = mp_hands.Hands(
     static_image_mode=False,
@@ -19,7 +32,7 @@ hands = mp_hands.Hands(
 )
 mp_drawing = mp.solutions.drawing_utils
 
-# Captura
+# --- CAPTURA DE VIDEO ---
 cap = cv2.VideoCapture(0)
 
 print("ESC para salir y guardar.")
@@ -42,7 +55,7 @@ while True:
             #vec son los 42 point
             X = np.array([vec])  # forma (1, 42)
             pred = model.predict(X, verbose=0)[0]
-            pred_label = labels[np.argmax(pred)]
+            pred_label = LABELS[np.argmax(pred)]
             confidence = np.max(pred)
             cv2.putText(frame,
                 f"{pred_label} ({confidence:.2f})",  # texto con clase + confianza
@@ -51,7 +64,7 @@ while True:
                 1,                                   # escala
                 (0, 255, 0),                         # color (verde)
                 2)                                   # grosor
-            print(pred_label)
+            #print(pred_label)
             for hand_landmarks in res.multi_hand_landmarks:
                 mp_drawing.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
         else:
---
+++ ejercicio2/train-gesture-classifier.py
@@ -0,0 +1,59 @@
+############################################################################################
+#                                                                                          #
+#                               APRENDIZAJE AUTOMÁTICO 2                                   #
+#                                 TRABAJO PRÁCTICO N°1                                     #
+#                                                                                          #
+#                GRUPO N°12: Ulises Nemeth, Agustín Alsop, Rocío Hachen                    #
+#                                                                                          #
+#                          Problema 2 - Piedra, Papel y Tijera                             #
+#                                                                                          #
+#                                 Generación de modelo                                     #
+############################################################################################
+
+# --- LIBRERÍAS ---
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Dropout
+from tensorflow.keras.utils import to_categorical
+from sklearn.model_selection import train_test_split
+
+# --- CARGA DE DATOS Y LABELS ---
+X = np.load("./dataset/rps_dataset.npy")   # (N, 42)
+y = np.load("./dataset/rps_labels.npy")    # (N,)
+
+# --- PROCESAMIENTO ---
+X = np.array(X).astype("float32")
+y = to_categorical(y, num_classes=3)  # one-hot encoding - rari no fue hecho por mi esta linea jejex
+
+# --- DIVISIÓN DE DATOS EN TEST Y TRAIN ---
+X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# --- DEFINICIÓN DE MODELO ---
+model = Sequential([
+    Dense(128, activation='relu', input_shape=(42,)),
+    Dropout(0.3),
+    Dense(64, activation='relu'),
+    Dropout(0.3),
+    Dense(32, activation='relu'),
+    Dropout(0.3),
+    Dense(3, activation='softmax')  # 3 clases: piedra, papel, tijera
+])
+
+model.compile(optimizer='adam',
+            loss='categorical_crossentropy',
+            metrics=['accuracy'])
+
+model.summary()
+
+# --- ENTRENAMIENTO ---
+history = model.fit(
+    X_train, y_train,
+    epochs=50,
+    batch_size=16,
+    validation_data=(X_val, y_val)
+)
+
+# --- GUARDAR EL MODELO ---
+model.save("./modelo/rps_model.h5")
+print("✅ Modelo guardado como rps_model.h5")
